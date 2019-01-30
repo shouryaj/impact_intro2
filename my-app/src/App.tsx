652bf86ff1790052156832d1a67e870f1c3cb37a
Joey Schafer <schaferj@cs.washington.edu>
2019-01-29 19:34:58 -0800

    Joeys changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -9,11 +9,6 @@ class App extends React.Component {
     public render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <Header name="Impact++" />
-                </header>
-                <Description countBy={3} />
                 <Headbar/>
                 <Sidebar/>
                 <Body/>
