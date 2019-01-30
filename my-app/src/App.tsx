0d97d791d93cef94a12bedbbd7c1d44e4c5bf04e
estberg <estbergm@uw.edu>
2019-01-29 19:44:08 -0800

    Fixed App.tsx to resolve merge conflicts

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import './App.css';
-<<<<<<< HEAD
+
 import Body from "./Body";
 import Headbar from "./Headbar";
 import Sidebar from "./Sidebar";
 import Description from "./Description";
-=======
-import Description from './Description';
 import SimpleCountButton from './SimpleCountButton';
-import Header from './Header';
-import logo from './logo.svg';
->>>>>>> mitchell_and_lea
 
 class App extends React.Component {
     public render() {
