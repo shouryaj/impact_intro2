a9d3ffd1caab260a5e40d4b50b59fc9a7effd16a
shourj@uw.edu <Pl@zmatv1>
2019-01-29 20:15:26 -0800

    Spotify try1

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,16 +3,17 @@ import './App.css';
 import Description from './Description';
 import Header from './Header';
 import logo from './logo.svg';
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     public render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                <header>
                     <Header name="Impact++" />
                 </header>
                 <Description countBy={8} />
+                <SearchBar />
             </div>
         );
     }
